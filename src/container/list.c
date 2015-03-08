746d0f072020d37d730f34d24056559b622ff046
Christof Pieloth <cpieloth@gmx.net>
2015-03-08 20:38:32 +0100

    [CHANGE] Refactor list.

---
+++ src/container/list.c
@@ -0,0 +1,124 @@
+﻿#include <limits.h> // INT_MIN
+#include <stdlib.h> // malloc
+#include <stdio.h> // fprintf, printf
+
+#include "list.h"
+
+const list_value_type list_NO_VALUE = INT_MIN;
+
+void list_init(list* const this)
+{
+	this->begin = NULL;
+	this->end = NULL;
+	this->size = 0;
+}
+
+void list_push_back(list* const this, const list_value_type value)
+{
+	list_node* e = malloc(sizeof(list_node));
+	e->value = value;
+	e->next = NULL;
+
+	if (list_empty(this) == list_TRUE)
+	{
+		this->begin = e;
+		this->end = e;
+	}
+	else
+	{
+		this->end->next = e;
+		this->end = e;
+	}
+	++this->size;
+}
+
+list_bool_type list_remove(list* const this, const list_value_type value)
+{
+	if (list_empty(this) == list_TRUE)
+	{
+		return list_FALSE;
+	}
+
+	list_node* it = this->begin;
+	list_node* prev = NULL;
+	do
+	{
+		if (it->value == value)
+		{
+			if (prev == NULL)
+				this->begin = it->next;
+			else
+				prev->next = it->next;
+			free(it);
+			--this->size;
+			return list_TRUE;
+		}
+		prev = it;
+		it = it->next;
+	} while (it != NULL);
+
+	return list_FALSE;
+}
+
+list_value_type list_at(const list* const this, list_size_type index)
+{
+	if (index >= list_size(this))
+	{
+		fprintf(stderr, "%s(): Index out of bounce!\n", __func__);
+		return list_NO_VALUE;
+	}
+
+	list_node* it = this->begin;
+	for (list_size_type i = 0; i < index; ++i)
+	{
+		it = it->next;
+	}
+	return it->value;
+}
+
+list_bool_type list_empty(const list* const this)
+{
+	if (!this->begin)
+		return list_TRUE;
+	else
+		return list_FALSE;
+}
+
+list_size_type list_size(const list* const this)
+{
+	return this->size;
+}
+
+void list_print(const list* const this, const char* const prefix)
+{
+	if (list_empty(this) == list_TRUE)
+	{
+		printf("%s[]\n", prefix);
+		return;
+	}
+	list_node* it = this->begin;
+	printf("%s[", prefix);
+	do
+	{
+		printf(" %d,", it->value);
+		it = it->next;
+	} while (it != NULL);
+
+	printf("]\n");
+}
+
+void list_clear(list* const this)
+{
+	list_node* it = this->begin;
+	list_node* toDel;
+	while (it != NULL)
+	{
+		toDel = it;
+		it = it->next;
+		free(toDel);
+		--this->size;
+	}
+
+	this->begin = NULL;
+	this->end = NULL;
+}
---
+++ src/container/list.h
@@ -0,0 +1,104 @@
+﻿/**
+ * Linked list for integers.
+ *
+ * \author cpieloth
+ */
+
+#ifndef CONTAINER_LIST_H_
+#define CONTAINER_LIST_H_
+
+#include <stddef.h> // size_t
+
+typedef enum
+{
+	list_FALSE, list_TRUE
+} list_bool_type;
+
+typedef size_t list_size_type; //!< Type for index and size.
+
+typedef int list_value_type; //!< Value type to be stored.
+
+extern const list_value_type list_NO_VALUE; //!< Indicates if an error or no value is returned.
+
+/**
+ * Internal value wrapper.
+ */
+typedef struct _list_node
+{
+	list_value_type value;
+	struct _list_node* next;
+} list_node;
+
+/**
+ * Structure to hold a list.
+ */
+typedef struct
+{
+	list_node* begin;
+	list_node* end;
+	list_size_type size;
+} list;
+
+/**
+ * Initializes a list instance.
+ *
+ * \param this A list structure.
+ */
+void list_init(list* const this);
+
+/**
+ * Appends an element at the list's end.
+ *
+ * \param this List instance.
+ * \param value Value to append.
+ */
+void list_push_back(list* const this, const list_value_type value);
+
+/**
+ * Removes first element which is found with the given value.
+ *
+ * \param this List instance.
+ * \param value value to delete
+ * \return list_TRUE if element was found and removed, else list_FALSE.
+ */
+list_bool_type list_remove(list* const this, const list_value_type value);
+
+/**
+ * Gets the element at the defined position.
+ *
+ * \param this List instance.
+ * \param index Postion in the list.
+ * \return The value at the index or list_NO_VALUE.
+ */
+list_value_type list_at(const list* const this, list_size_type index);
+
+/**
+ * Check if the list is empty.
+ *
+ * \param this List instance.
+ * \return list_TRUE if the given list is empty, else list_FALSE.
+ */
+list_bool_type list_empty(const list* const this);
+
+/**
+ * Get element count.
+ *
+ * \param this List instance.
+ */
+list_size_type list_size(const list* const this);
+
+/**
+ * Prints the list on stdout.
+ *
+ * \param this List instance.
+ */
+void list_print(const list* const this, const char* const prefix);
+
+/**
+ * Remove all entries from the list. Does not free the values!
+ *
+ * \param this List instance.
+ */
+void list_clear(list* const this);
+
+#endif /* CONTAINER_LIST_H_ */
